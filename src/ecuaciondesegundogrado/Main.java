f0158380391461d2c5a04b3baf7fca44033e5fef
Moisés Santiago Canché Pool <16470283@itcampeche.edu.mx>
2017-06-11 22:59:00 -0500

    Version final del proyecto

--- src/ecuaciondesegundogrado/Main.java
+++ src/ecuaciondesegundogrado/Main.java
@@ -5,6 +5,9 @@
  */
 package ecuaciondesegundogrado;
 
+import java.util.Scanner;
+import modelo.EcuacionDeSegundoGrado;
+
 /**
  *
  * @author moises santiago
@@ -15,7 +18,26 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner entrada = new Scanner(System.in);
+       EcuacionDeSegundoGrado ecuacion1 = new EcuacionDeSegundoGrado(3,9,2);
+       System.out.println("valor discriminante:" + ecuacion1.getDiscriminante());
+       System.out.println("numero de raices:" + ecuacion1.getNumeroDeSolucionesReales());
+       System.out.println("x1 = "+ ecuacion1.getRaiz1());
+       System.out.println("x2 = "+ ecuacion1.getRaiz2());
+       
+       
+       
+       EcuacionDeSegundoGrado ecuacion2 = new EcuacionDeSegundoGrado(6,9,1);
+       System.out.println("valor discriminante:" + ecuacion2.getDiscriminante());
+       System.out.println("numero de raices:" + ecuacion2.getNumeroDeSolucionesReales());
+       System.out.println("x1 = "+ ecuacion2.getRaiz1());
+       System.out.println("x2 = "+ ecuacion2.getRaiz2());
+       
+       EcuacionDeSegundoGrado ecuacion3 = new EcuacionDeSegundoGrado(8,3,-6);
+       System.out.println("valor discriminante:" + ecuacion3.getDiscriminante());
+       System.out.println("numero de raices:" + ecuacion3.getNumeroDeSolucionesReales());
+       System.out.println("x1 = "+ ecuacion3.getRaiz1());
+       System.out.println("x2 = "+ ecuacion3.getRaiz2());
     }
     
 }
--- src/modelo/EcuacionDeSegundoGrado.java
+++ src/modelo/EcuacionDeSegundoGrado.java
@@ -10,5 +10,54 @@ package modelo;
  * @author moises santiago
  */
 public class EcuacionDeSegundoGrado {
-    
+    public double a;
+    public double b;
+    public double c;
+   
+public EcuacionDeSegundoGrado ( double a, double b, double c){
+    this.a=a;
+    this.b=b;
+    this.c=c;      
+}
+public double getCoeficienteA(){
+    return a;
+}
+public double getCoeficineteB(){
+    return b;
+}
+public double getCoeficienteC(){
+    return c;
+}
+public double getDiscriminante(){
+    double d = (b*b)-4*a*c;
+    return d;
+}
+public double getNumeroDeSolucionesReales(){
+    double dt = getDiscriminante();
+    int NumeroDeRaiz;
+    if (dt > 0){
+        NumeroDeRaiz = 2;
+    }
+    else 
+        if(dt <0){
+            NumeroDeRaiz = 0;
+        }
+    else
+        {
+            NumeroDeRaiz = 1;
+        }
+       return NumeroDeRaiz;
+      
+}
+public double getRaiz1(){
+    double raiz1;
+    raiz1 = (-b + Math.sqrt(getDiscriminante()))/ (2*a);
+    return raiz1;
+}
+public double getRaiz2(){
+    double raiz2;
+    raiz2 = (-b - Math.sqrt(getDiscriminante()))/ (2*a);
+    return raiz2;
+}
+
 }
